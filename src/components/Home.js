c5188edd5e6d1dadba3eb9ccb39de497b9f1e7fe
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-07-30 13:52:33 +0700

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../firebase";
 import { Redirect } from "react-router-dom";
 // import { useCollection } from "react-firebase-hooks/firestore";
+import ServerIcon from './ServerIcon';
 
 const Home = () => {
     const [user] = useAuthState(auth);
@@ -19,7 +20,22 @@ const Home = () => {
     // };
 
   return (
-    <div>Home</div>
+    <>
+      {!user && <Redirect to="/" />}
+      <div className="flex h-screen">
+        <div className="flex flex-col space-y-3 bg-[#202225] p-3 min-w-max">
+          <div className="server-default hover:bg-discord_purple">
+            <img src="https://rb.gy/kuaslg" alt="" className="h-5" />
+          </div>
+          <hr className=" border-gray-700 border w-8 mx-auto" />
+          <ServerIcon image="https://rb.gy/qidcpp" />
+          <ServerIcon image="https://rb.gy/zxo0lz" />
+          <ServerIcon image="https://rb.gy/qidcpp" />
+          <ServerIcon image="https://rb.gy/zxo0lz" />
+         
+        </div>
+      </div>
+    </>
   )
 }
 
