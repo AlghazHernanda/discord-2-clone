3d36b380d0507e9b759b189914c1473ca0970fad
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-07-30 15:44:35 +0700

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
 
   return (
     <>
-      {!user && <Redirect to="/" />}
+      {/* {!user && <Redirect to="/" />} */}
       <div className="flex h-screen">
         <div className="flex flex-col space-y-3 bg-[#202225] p-3 min-w-max">
           <div className="server-default hover:bg-discord_purple">
