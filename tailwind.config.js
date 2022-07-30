b5c6e29b09b655e74478c887fed08bdd0fba41b3
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-07-30 15:44:24 +0700

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         discord_blurple: "#7289da",
         discord_purple: "#5865f2",
         discord_green: "#3ba55c",
+        discord_serverBg: "#36393f",
       },
     },
   },
