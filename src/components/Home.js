61be5a91af6b96085430e1757507505aaabfb099
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-07-31 13:18:31 +0700

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ChevronDownIcon, PlusIcon } from "@heroicons/react/outline";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../firebase";
 import { Redirect } from "react-router-dom";
@@ -32,8 +33,28 @@ const Home = () => {
           <ServerIcon image="https://rb.gy/zxo0lz" />
           <ServerIcon image="https://rb.gy/qidcpp" />
           <ServerIcon image="https://rb.gy/zxo0lz" />
-         
+
+          <div className="server-default hover:bg-discord_green group">
+            <PlusIcon className="text-discord_green h-7 group-hover:text-white" />
+          </div>
         </div>
+
+        <div className="bg-[#2f3136] flex flex-col min-w-max">
+        <h2 className="flex text-white font-bold text-sm items-center justify-between border-b border-gray-800 p-4 hover:bg-[#34373C] cursor-pointer">
+            Official PAPA Server... <ChevronDownIcon className="h-5 ml-2" />
+          </h2>
+          <div className="text-[#8e9297] flex-grow overflow-y-scroll scrollbar-hide">
+            <div className="flex items-center p-2 mb-2">
+                <ChevronDownIcon className="h-3  mr-2" />
+                <h4 className="font-semibold ">Channels</h4>
+                <PlusIcon
+                  className="h-6 ml-auto cursor-pointer hover:text-white"
+                  // onClick={handleAddChannel}
+                />
+              </div>
+          </div>
+        </div>
+
       </div>
     </>
   )
