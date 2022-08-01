f122fcb7e3db5c8ce77d4b832590417d8c5a3064
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-08-01 14:10:21 +0700

    update

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,11 +1,11 @@
 import { HashtagIcon } from "@heroicons/react/outline";
-// import { useDispatch } from "react-redux";
-// import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 // import { setChannelInfo } from "../features/channelSlice";
 
 function Channel({ id, channelName }) {
-  // const dispatch = useDispatch();
-  // const history = useHistory();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   // const setChannel = () => {
   //   dispatch(
