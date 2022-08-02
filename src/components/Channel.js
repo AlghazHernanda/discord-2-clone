86997cd3c3fb4c9a1b1f4ea46a95060aa0c061a4
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-08-02 17:04:04 +0700

    channel

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,27 +1,27 @@
 import { HashtagIcon } from "@heroicons/react/outline";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
-// import { setChannelInfo } from "../features/channelSlice";
+import { setChannelInfo } from "../features/channelSlice";
 
 function Channel({ id, channelName }) {
   const dispatch = useDispatch();
   const history = useHistory();
 
-  // const setChannel = () => {
-  //   dispatch(
-  //     setChannelInfo({
-  //       channelId: id,
-  //       channelName: channelName,
-  //     })
-  //   );
+  const setChannel = () => {
+    dispatch(
+      setChannelInfo({
+        channelId: id,
+        channelName: channelName,
+      })
+    );
 
-  //   history.push(`/channels/${id}`);
-  // };
+    history.push(`/channels/${id}`);
+  };
 
   return (
     <div
       className="font-medium flex items-center cursor-pointer hover:bg-[#3A3C43] p-1 rounded-md  hover:text-white"
-      //onClick={setChannel}
+      onClick={setChannel}
     >
       <HashtagIcon className="h-5 mr-2" /> {channelName}
     </div>
